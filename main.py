e30aa592ca2f4a85ba332912d97104e4b526c7cd
MilesWK <mileswkrueger@gmail.com>
2026-09-06 18:20:11 -0400

    Make it. It is terrifying

--- main.py
+++ main.py
@@ -11,9 +11,41 @@ app = App(
 
 
 @app.command("/impersonate")
-def impersonate(ack, command, client):
-    # stuff will go here...
-    pass
+def impersonate(ack, command, client, respond):
+    ack()
+    uti = command["text"].split(" ")[0].replace("@", "") # UTI: USER TO IMPERSONATE :)
+    msg = command["text"].split(' ', 1)[1]
+
+    print(uti)
+
+    channel_id = command["channel_id"]
+
+    result = client.conversations_members(channel=channel_id)
+
+    for user_id in result["members"]:
+        print(user_id)
+        user = client.users_info(user=user_id)["user"]
+        print(user["name"])
+        if uti in user["name"]:
+            utiid = user_id
+            utiimg = user["profile"]["image_192"]
+            utiname = user["profile"]["display_name"]
+            print(utiimg)
+            break
+
+    if utiid:
+        client.chat_postMessage(
+            channel=command["channel_id"],
+            text=msg,
+            response_type="ephemeral",
+            icon_url=utiimg,
+            username=utiname
+        )
+    else:
+        respond(
+            text="That user isn't in this channel.",
+            response_type="ephemeral"
+        )
 
 
 if __name__ == "__main__":
