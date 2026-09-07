76be7863dc0f7eb6446892047c27eaa365197ab6
MilesWK <mileswkrueger@gmail.com>
2026-09-06 20:08:16 -0400

    Redo ID-Finding system for speed and overall awesomeness

--- main.py
+++ main.py
@@ -15,19 +15,17 @@ def impersonate(ack, command, client, respond):
     ack()
     uti = command["text"].split(" ")[0].replace("@", "") # UTI: USER TO IMPERSONATE :)
     msg = command["text"].split(' ', 1)[1]               # Everything that isn't the UTI
-
     print(uti) # debug stuff
-
+    utiid = ""
     channel_id = command["channel_id"] # get the channel.
 
     result = client.conversations_members(channel=channel_id) # get all the users in the channel
-    for user_id in result["members"]: # for each id in that list,
-        user = client.users_info(user=user_id)["user"] # find the user with that id
-        if uti in user["name"]: # if the id matches up with the User we want to impersonate
-            utiid = user_id # set the UTI-ID to that user's id
-            utiimg = user["profile"]["image_192"] # get the image of the user
-            utiname = user["profile"]["display_name"] # and get the name.
-            break # and stop. We don't need to look any further.
+    utiid = uti.replace("<", "").split("|")[0]
+    print(utiid)
+
+    user = client.users_info(user=utiid)["user"]
+    utiimg = user["profile"]["image_192"] # get the image of the user
+    utiname = user["profile"]["display_name"] # and get the name.
     if utiid == "U0A5EEQ1RFY":
         respond( # send an error.
                     text="You can't impersonate Miles, silly.",
